cc6a567938c181dd7ae569fd82c5cb049a37df1f
Yassine El motaouakkel <yel-mota@c1r9p10.1337.ma>
2026-06-20 18:05:31 +0100

    the half work done need some test's and add the last piece location block-level

--- parce.cpp
+++ parce.cpp
@@ -1,5 +1,6 @@
 #include "webserv.h"
 #include <csetjmp>
+#include <cmath>
 
 // NOTE: skiping comment's and delete new line
 // NOTE: Work on brakets {} and a functionality of nested brakest
@@ -100,7 +101,7 @@ void ft_port_ip_geter(std::string &it, t_config & t)
 		t.ip = it.substr(0, i);
 		ft_check_ip(t.ip);
 		t.port = std::strtod(it.substr(i + 1).c_str() , &c);
-		if (c != '\0')
+		if (*c != '\0')
 			throw std::runtime_error("port should contain only numbers");
 	}
 	else {
@@ -112,10 +113,117 @@ void ft_port_ip_geter(std::string &it, t_config & t)
 		}
 		else {
 			t.port = std::strtod(it.c_str() , &c);
-			if (c != '\0')
+			if (*c != '\0')
 				throw std::runtime_error("port should contain only numbers");
 		}
 	}
+}
+
+bool ft_check_is_number(std::string &it)
+{
+	size_t	i;
+
+	i = 0;
+	while (it[i])
+	{
+		if (!std::isdigit(it[0]))
+			return (false);
+	}
+	return (true);
+}
+
+int	ft_to_number(std::string &it)
+{
+	size_t i = 0;
+	size_t j = 0;
+
+	while (it[i])
+	{
+		j = j * 10 + it[i] - '0';
+	}
+	return (j);
+}
+
+void ft_error_pages_get(std::vector< std::string >::iterator &it, std::vector<std::string>::iterator &end, t_config &t)
+{
+	std::vector<int>	v_error;
+	std::string			s_error = "";
+	size_t				tmp_error = 0;
+
+	while (*it != ";")
+	{
+		if (!s_error.empty())
+		{
+			throw std::runtime_error("In error_page after the path it should have dilimeter");
+		}
+		if (ft_check_is_number(*it))
+		{
+			tmp_error = ft_to_number(*it);
+			if (tmp_error < 300 || tmp_error >= 600)
+				throw std::runtime_error("Invalide error code (the error code should be arround 300-599)");
+			v_error.push_back(tmp_error);
+		}
+		else {
+			s_error = *it;
+		}
+		it++;
+		if (it == end)
+			throw std::runtime_error("The variable should followed by a value and a dilimeter");
+	}
+	if (!v_error.size())
+	{
+		throw std::runtime_error("error_pages should contain a error numbers");
+	}
+	if (s_error.empty())
+	{
+		throw std::runtime_error("error_pages should have the path to the error display");
+	}
+	for (std::vector<int>::iterator iv = v_error.begin(); iv != v_error.end(); iv++)
+	{
+		t.error_pages[*iv] = s_error;
+	}
+}
+
+void ft_methods_get(std::vector<std::string>::iterator & it, std::vector<std::string>::iterator & end, t_config & t)
+{
+	if (*it == ";")
+		throw std::runtime_error("The allow_methods variable should have at least one allow method (POLL, GET or DELETE)");
+	t.allow_method = 0;
+	while (*it != ";")
+	{
+		if (*it == "GET")
+			t.allow_method |= GET;
+		else if (*it == "DELETE")
+			t.allow_method |= DELETE;
+		else if (*it == "POLL")
+			t.allow_method |= POLL;
+		else
+		 	throw std::runtime_error("The mothod's gaving allowe_methods are invalide");
+		it++;
+		if (it == end)
+			throw std::runtime_error("Every variable end with delimeter ';'");
+	}
+}
+
+void ft_get_max_body(std::string &s, t_config &t)
+{
+	size_t	size;
+	char	*c;
+	short	p;
+
+	size = std::strtoul(s.c_str(), &c, 10);
+	if (*c == '\0')
+		p = 0;
+	else if (*c == 'K' || *c == 'k')
+		p = 1;
+	else if (*c == 'M' || *c == 'm')
+		p = 2;
+	else if  (*c == 'G' || *c == 'g')
+		p = 3;
+	else
+		throw std::runtime_error("Invalide character in max body");
+	//WARN: should handle the overflow
+	t.client_max_body_size = size * std::pow(2, p * 10);
 }
 
 void	ft_handle_ather(std::vector<std::string>::iterator &it, std::vector<std::string>::iterator &end, t_config &t)
@@ -123,12 +231,13 @@ void	ft_handle_ather(std::vector<std::string>::iterator &it, std::vector<std::st
 	if (*it == "listen")
 	{
 		it++;
-		if (*it == ";")
+		if (it == end || *it == ";")
 		{
 			throw std::runtime_error("Non value gaving for listen");
 		}
 		ft_port_ip_geter(*it, t);
-		if (*it != ";")
+		it++;
+		if (end == it || *it != ";")
 		{
 			throw std::runtime_error("Every variable should end with delimeter ';'");
 		}
@@ -136,27 +245,100 @@ void	ft_handle_ather(std::vector<std::string>::iterator &it, std::vector<std::st
 	else if (*it == "server_name")
 	{
 		it++;
-		if (*it == ";")
+		if (end == it || *it == ";")
 		{
 			throw std::runtime_error("Non value gaving for server_name");
 		}
 		while (*it != ";")
 		{
-			if (it == end)
-				throw std::runtime_error("Every variable end with delimeter ';'");
-			// WARN: this doesn't check if the actual server name is valid
 			t.server_name.push_back(*it);
 			it++;
+			if (it == end)
+				throw std::runtime_error("Every variable end with delimeter ';'");
 		}
 	}
 	else if (*it == "client_max_body_size")
 	{
-
+		it++;
+		if (it == end || *it == ";")
+		{
+			throw std::runtime_error("Non value gaving for client_max_body_size");
+		}
+		ft_get_max_body(*it, t);
+		it++;
+		if (end == it || *it != ";")
+		{
+			throw std::runtime_error("Every variable should end with delimeter ';'");
+		}
 	}
 	else if (*it == "error_page")
 	{
+		it++;
+		if (it == end || *it == ";")
+		{
+			throw std::runtime_error("Non value gaving for error_page");
+		}
+		ft_error_pages_get(it, end, t);
+	
+	}
+	else if (*it == "root")
+	{
+		it++;
+		if (it == end || *it == ";")
+		{
+			throw std::runtime_error("Non value gaving for root");
+		}
+		t.root_path = *it;
+		it++;
+		if (end == it || *it != ";")
+		{
+			throw std::runtime_error("Every variable should end with delimeter ';'");
+		}
+	}
+	else if (*it == "index")
+	{
+		it++;
+		if (it == end || *it == ";")
+		{
+			throw std::runtime_error("Non value gaving for index");
+		}
+		t.index = *it;
+		it++;
+		if (end == it || *it != ";")
+		{
+			throw std::runtime_error("Every variable should end with delimeter ';'");
+		}
+	}
+	else if (*it == "allow_methods")
+	{
+		it++;
+		if (it == end || *it == ";")
+		{
+			throw std::runtime_error("Non value gaving for allow_methods");
+		}
+		ft_methods_get(it, end, t);
+	}
+	else if (*it == "autoindex")
+	{
+		it++;
+		if (it == end || *it == ";")
+		{
+			throw std::runtime_error("Non value gaving for index");
+		}
+		if (*it == "on" || *it == "ON")
+			t.autoindex = true;
+		else if (*it == "off" || *it == "OFF")
+			t.autoindex = false;
+		else
+		 	throw std::runtime_error("In autoindex it should have eather on/off");
+		it++;
+		if (end == it || *it != ";")
+		{
+			throw std::runtime_error("Every variable should end with delimeter ';'");
+		}
 	}
 	else {
+		std::cerr << "The variable : " + *it << std::endl;
 		throw std::runtime_error("Invalide variable in the config file");
 	}
 }
@@ -173,14 +355,15 @@ t_config	ft_full_server_config(std::vector<std::string> &s_v)
 	{
 		throw std::runtime_error("The block diractive should start have a starting breaket");
 	}
-	for (std::vector<std::string>::iterator it = s_v.begin(); it != s_v.end(); it++)
+	for (std::vector<std::string>::iterator it = s_v.begin() + 2; it != s_v.end(); it++)
 	{
 		if (*it == "location")
 		{
 			// ft_handle_location(it, t);
 		}
 		else {
-			// ft_handle_ather(it, t);
+			std::vector<std::string>::iterator iv(  s_v.end() );
+			ft_handle_ather(it, iv, t);
 		}
 	}
 	return (t);
@@ -206,7 +389,7 @@ t_config	ft_parce_config(const char *path_to_config)
 			std::cout << "line : " << i << " " << *it << std::endl;
 			i++;
 		}
-		// v_config.push_back(ft_full_server_config(v_s));
+		v_config.push_back(ft_full_server_config(v_s));
 		v_s.clear();
 	}
 	is.close();
--- webserv.h
+++ webserv.h
@@ -11,12 +11,23 @@
 # include <stack>
 # include <exception>
 
+typedef	enum e_method {
+	GET = 1,
+	POLL = 2,
+	DELETE = 4,
+	UNKNOWN = 8
+}	t_method;
+
 typedef struct s_config {
 	std::vector<std::string>		server_name;
 	std::map<int, std::string>	error_pages;
 	size_t						client_max_body_size;
 	size_t						port;
 	std::string					ip;
+	std::string					root_path;
+	std::string					index;
+	bool					autoindex;
+	short						allow_method;
 } t_config;
 
 t_config	ft_parce_config(const char *path_to_config);
